0b162c990492b2f4a83646b78cc460c28cdd2be9
Steven Silvester <steven.silvester@ieee.org>
2016-01-11 13:05:29 -0600

    Update build infrastructure

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
 npm install
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-conda install jupyter
-pip install -U notebook
-
-# create jupyter base dir (needed for config retreival)
-mkdir ~/.jupyter
